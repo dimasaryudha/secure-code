7446871959682f3e86ad3940185d545eaeb265b9
dimasaryudha <dimasaryudha@smkwikrama.sch.id>
2026-04-17 10:52:57 +0700

    Setup GitHub Actions CI

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Welcome</title>
+</head>
+<body>
+
+    <h1>Test</h1>
+
+    <a href="{{ route('login') }}">
+        <button>Login</button>
+    </a>
+
+</body>
+</html>
